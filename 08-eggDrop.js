70113cb852857c9a0e7ff289d4f332022eaeaa61
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-03-26 23:44:15 -0400

    Optional assignment

---
+++ 08-eggDrop.js
@@ -0,0 +1,11 @@
+// This is a fun exercise to do - consider this optional
+// after you are done with all the exercises above. Imagine
+// that you wanted to find the highest floor of a 100 story
+// building that you could drop an egg from without the egg
+// breaking. But you only have 2 eggs. Write an algorithm to
+// find out in the most efficient way which floors you should
+// drop the eggs from. After you have understood the question
+// and made some attempts to solve the problem, go through
+// this reading before you start coding:
+
+// http://datagenetics.com/blog/july22012/index.html.
