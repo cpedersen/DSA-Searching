d921d9fc7c66c2d92af111f5d955a5a7394b913d
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-03-28 00:30:46 -0400

    Assignment 8

--- 08-eggDrop.js
+++ 08-eggDrop.js
@@ -9,3 +9,32 @@
 // this reading before you start coding:
 
 // http://datagenetics.com/blog/july22012/index.html.
+
+function eggDrop(
+  eggs = 2,
+  breakPt = Math.floor(Math.random() * 100),
+  currFloor = 0
+) {
+  if (eggs === 0) {
+    console.log(
+      `The breakpoint was ${breakPt} and the highest safe floor was ${currFloor}`
+    );
+    return currFloor;
+  }
+  if (eggs === 1) {
+    if (currFloor + 1 >= breakPt) {
+      eggDrop(eggs - 1, breakPt, currFloor);
+    } else {
+      eggDrop(eggs, breakPt, currFloor + 1);
+    }
+  }
+  if (eggs === 2) {
+    if (currFloor + 14 > breakPt) {
+      eggDrop(eggs - 1, breakPt, currFloor);
+    } else {
+      eggDrop(eggs, breakPt, currFloor + 14);
+    }
+  }
+}
+
+eggDrop();
